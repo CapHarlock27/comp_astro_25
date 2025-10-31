8811fbc9faac515825621c633f39885ea76d3b5c
PhobosII <nicholas.friso@studenti.unipd.it>
2025-10-31 21:54:50 +0000

    Updated main for task F and G of assignment 1.

--- src/daneel/__main__.py
+++ src/daneel/__main__.py
@@ -7,39 +7,40 @@ from daneel.detection import *
 def main():
     parser = argparse.ArgumentParser()
 
-    # parser.add_argument(
-    #     "-i",
-    #     "--input",
-    #     dest="input_file",
-    #     type=str,
-    #     required=True,
-    #     help="Input par file to pass",
-    # )
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest = "input_file",
+        type = str,
+        required = True,
+        help = "Input parameter file to pass",
+    )
 
     parser.add_argument(
         "-t",
         "--transit",
         dest = "transit",
-        required = False,
+        required = False,        
+        help = "Plot the light curve of the selected exoplanet",
         action = "store_true",
     )
 
     # parser.add_argument(
     #     "-d",
     #     "--detect",
-    #     dest="detect",
-    #     required=False,
-    #     help="Initialise detection algorithms for Exoplanets",
-    #     action="store_true",
+    #     dest = "detect",
+    #     required = False,
+    #     help = "Initialise detection algorithms for Exoplanets",
+    #     action = "store_true",
     # )
 
     # parser.add_argument(
     #     "-a",
     #     "--atmosphere",
-    #     dest="complete",
-    #     required=False,
-    #     help="Atmospheric Characterisazion from input transmission spectrum",
-    #     action="store_true",
+    #     dest ="complete",
+    #     required = False,
+    #     help = "Atmospheric Characterisazion from input transmission spectrum",
+    #     action = "store_true",
     # )
 
     args = parser.parse_args()
@@ -48,15 +49,15 @@ def main():
     start = datetime.datetime.now()
     print(f"Daneel starts at {start}")
 
-    # input_pars = Parameters(args.input_file).params
+    input_params = Parameters(args.input_file).params
 
     # if args.detect:
     #     pass
     # if args.atmosphere:
     #     pass
     if args.transit:
-        obj = TransitModel()
-        obj.plot()
+        new_obj = TransitModel(input_params)
+        new_obj.plot()
 
     finish = datetime.datetime.now()
     print(f"Daneel finishes at {finish}")
